8c89dcc6514baf83c137976eac537e6a0b0375ad
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2025-05-22 23:59:16 +0530

    creating loginpage

--- client/src/components/Messenger.jsx
+++ client/src/components/Messenger.jsx
@@ -1,7 +1,28 @@
+import LoginDialog from "./account/LoginDialog";
+import { AppBar, Toolbar,styled, Box } from "@mui/material";
 
+const Component = styled(Box)`
+  height: 100vh;
+  background: #DCDCDC;
+`
+const Header = styled(AppBar)`
+  height: 220px;
+  background-color: #00bfa5;
+  box-shadow: none;
+`
 
 const Messenger = () => {
-  return <p> Hello from Messenger</p>
-}
+
+  return(
+  <Component>
+   <Header>
+    <Toolbar>
+
+    </Toolbar>
+  </Header>
+  <LoginDialog />
+  </Component>
+
+)}
 
 export default Messenger;
