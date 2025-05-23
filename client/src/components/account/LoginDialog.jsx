a74f008880011eeebfe77e3ff26c8c34733b0e9f
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2025-05-23 13:19:18 +0530

    made the green background and front box

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,10 +1,40 @@
-
 import { Dialog } from "@mui/material";
-const LoginDialog = () => {
 
+const dialogStyle = {
+  height: '96%',
+  marginTop: '12%',
+  width: '60%',
+  maxWidth: '100%',
+  maxHeight: '100%',
+  boxShadow: 'none',
+  overflow: 'none',
+};
+
+const LoginDialog = () => {
   return (
-  <Dialog open = {true}>Hello</Dialog>
-  )
-}
+    <Dialog
+      open={true}
+      slotProps={{
+        paper: {
+          sx: dialogStyle,
+        },
+      }}
+    >
+    <Box>
+      <Box>
+    
+
+      </Box>
+      <Box>
+      
+
+      </Box>
+
+
+    </Box>
+    Hello
+    </Dialog>
+  );
+};
 
 export default LoginDialog;
