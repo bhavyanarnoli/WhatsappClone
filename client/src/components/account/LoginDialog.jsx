5ca07d5275d71d377f7b00d1061b95b6fc671295
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2025-05-23 15:58:27 +0530

    added qr code for login page

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,4 +1,6 @@
-import { Dialog } from "@mui/material";
+import { Dialog, Box, Typography, List, ListItem, styled } from "@mui/material";
+import { qrCodeImage } from "../constants/data";
+import { GoogleLogin } from '@react-oauth/google';
 
 const dialogStyle = {
   height: '96%',
@@ -7,8 +9,38 @@ const dialogStyle = {
   maxWidth: '100%',
   maxHeight: '100%',
   boxShadow: 'none',
-  overflow: 'none',
+  overflow: 'hidden',
 };
+
+const Component = styled(Box)`
+  display: flex;
+`
+const LeftContainer = styled(Box)`
+  padding: 56px 0 56px 56px;
+`
+const QrCode = styled('img')({
+  height: 264,
+  width: 264,
+  margin: '50px 0 0 50px'
+  })
+
+const Title = styled(Typography)`
+  font-size: 26px;
+  color: #525252;
+  font-weight: 300;
+  font-family: inherit;
+  margin-bottom: 25px;
+`;
+
+const styledList = styled(List)`
+  & > li {
+    padding: 0;
+    margin-top: 15px;
+    font-size: 18px;
+    line-height: 28px;
+    color: #4a4a4a;
+  }
+`
 
 const LoginDialog = () => {
   return (
@@ -20,19 +52,23 @@ const LoginDialog = () => {
         },
       }}
     >
-    <Box>
+    <Component>
+      <LeftContainer>
+        <Title> To use Whatsapp on your computer</Title>
+        <styledList>
+          <ListItem> 1. Open Whatsapp on your phone </ListItem>
+          <ListItem> 2.Tap menu Settings and select Whatsapp Web </ListItem>
+          <ListItem> 3. Point your phone to this screen to capture the code </ListItem>
+        </styledList>
+      </LeftContainer>
       <Box>
-    
+      <QrCode src = {qrCodeImage} alt = "QR Code" />
 
       </Box>
-      <Box>
-      
-
-      </Box>
 
 
-    </Box>
-    Hello
+    </Component>
+
     </Dialog>
   );
 };
